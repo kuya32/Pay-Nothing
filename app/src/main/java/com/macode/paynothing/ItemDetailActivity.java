b3168c98b5c43a29ea625d82a65342656c1f13bc
Marchael Acode <m.acode@outlook.com>
2021-02-17 01:43:16 -0800

    Adding location to the item post. Saving constructed item to the database. Also passing item data to item detail activity.

---
+++ app/src/main/java/com/macode/paynothing/ItemDetailActivity.java
@@ -0,0 +1,14 @@
+package com.macode.paynothing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+    }
+}
